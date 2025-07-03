2b2b726e8e50f1a25e139b1b6bafa926eae8aa0a
Zhiyuanlu000217 <lu000217@algonquinlive.com>
2025-07-03 18:02:49 -0400

    Fix Terraform syntax error - change region to location

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # cst8918-lab9
+
+I found out the tutorial provided command is not enough, I run additional steps:
+- npm run prepare
+- npx husky init
+
+Then it works
--- terraform/main.tf
+++ terraform/main.tf
@@ -6,9 +6,5 @@ provider "azurerm" {
 # Create a resource group
 resource "azurerm_resource_group" "example" {
   name     = "cst8918-lab9-rg"
-  region = "East US"
-  tags = {
-    Environment = "Production"
-    Owner       = "DevOps Team"
-  }
+  location = "East US"
 }
